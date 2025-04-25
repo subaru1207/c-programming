06568322d555c79c680e1244e7f5f07905caf1e1
N.S <balsat0w0@gmail.com>
2025-04-25 06:22:32 +0000

    4_25

---
+++ 4_25/4_25_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+number=8765;
+printf("number= %d\n",number);
+
+    return 0;
+}
---
+++ 4_25/4_25_4.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main(void) {
+    int num;
+    scanf("%d", &num);
+    if (num % 2 == 0) {
+        printf("%d は偶数です.\n",num );
+    } else {
+        printf("%d は奇数です.\n",num );
+    }
+     return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
