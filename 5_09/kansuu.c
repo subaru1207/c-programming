aaa5e17d92a59ad67cc566a4a27757b7ce78a8ec
N.S <balsat0w0@gmail.com>
2025-05-09 06:15:29 +0000

    5_02

---
+++ 5_09/kansuu.c
@@ -0,0 +1,25 @@
+// C言語の関数について
+// 特定の処理をまとめたプログラム
+#include <stdio.h>
+
+int add ();
+
+int main(void){
+    printf("%d\n", add(3));
+
+}
+
+//メイン関数は、プログラム全体で最初に実行される関数
+//私たちが作った関数は、すべてメイン関数で実行される
+
+int add (int a){
+    a=a + b;
+    
+}
+//関数はリターンを書く
+
+int add5(int a){
+    a= a + 5;
+    return a;
+}
+ 
